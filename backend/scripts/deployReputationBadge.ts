e2bc85ca804111fb78d5191957ed47dbc19a31b1
olivier4429 <obervas@yahoo.com>
2026-04-02 11:03:51 +0200

    Include SVG content in ReputationBadge deployment
    Added SVG content as an argument when deploying the ReputationBadge contract.

--- backend/scripts/deployReputationBadge.ts
+++ backend/scripts/deployReputationBadge.ts
@@ -1,11 +1,15 @@
 import { network } from "hardhat";
-import { formatEther } from "viem";
 const { viem, networkName } = await network.connect();
 const client = await viem.getPublicClient();
 
 console.log(`Deploying ReputationBadge to ${networkName}...`);
+//Lecture du svg
+const svgContent = readFileSync(
+  resolve(process.cwd(), "svg", "reputation-nft.mint.svg"),
+  "utf-8"
+);
 
-const nftContract = await viem.deployContract("ReputationBadge");
+const nftContract = await viem.deployContract("ReputationBadge", [svgContent]);
 
 console.log("ReputationBadge address:", nftContract.address);
 const owner = await nftContract.read.owner();
